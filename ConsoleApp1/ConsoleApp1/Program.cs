fa3a68e57e7014fa4f7e2bed2298c5b858f41586
stefanmaric01 <stefanmaric01@gmail.com>
2025-03-07 17:45:10 +0100

    Reverse string

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿static String reverse(string s)
+{
+
+    int left = 0;
+    int right = s.Length - 1;
+    char[] reversed = new char[s.Length];
+
+    while (left < s.Length)
+    {
+        reversed[left++] = s[right--];
+    }
+
+    Console.WriteLine(reversed);
+    return reversed.ToString();
+}
+
+reverse("Stefan");
