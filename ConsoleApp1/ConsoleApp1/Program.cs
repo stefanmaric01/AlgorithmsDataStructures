dc55ea1bee58c3537dd3a0f97a75301805537e6e
stefanmaric01 <stefanmaric01@gmail.com>
2025-03-08 11:52:25 +0100

    Is Palindrom

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -16,23 +16,42 @@
 
 //reverse("Stefan");
 
-static string isAnagram (string a, string b)
-{
+//static string isAnagram (string a, string b)
+//{
 
-    if (a.Length != b.Length)
-    {
+//    if (a.Length != b.Length)
+//    {
        
-        return "Is not anagram";
+//        return "Is not anagram";
 
+//    }
+
+//    char[] aChar = a.ToLower().ToCharArray();
+//    char[] bChar = b.ToLower().ToCharArray();
+
+//    Array.Sort(aChar);
+//    Array.Sort(bChar);
+
+//   return aChar.SequenceEqual(bChar) ? "IsAnagram" : "Is not Anagram";
+
+//}
+//isAnagram("Ana", "naa");
+
+
+static string isPalindrom (string s)
+{
+    int left = 0;
+    int right = s.Length - 1;
+    s = s.ToLower();
+    while(left <= right)
+    {
+        if (s[left] != s[right]) return "Not palindrom";
+        left++;
+        right--;
     }
+    return "is Palindrom";
 
-    char[] aChar = a.ToLower().ToCharArray();
-    char[] bChar = b.ToLower().ToCharArray();
-
-    Array.Sort(aChar);
-    Array.Sort(bChar);
-
-   return aChar.SequenceEqual(bChar) ? "IsAnagram" : "Is not Anagram";
-
+   
 }
-isAnagram("Ana", "naa");
+
+Console.WriteLine( isPalindrom("Anab"));
