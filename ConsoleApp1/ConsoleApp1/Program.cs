e66f576a3867e1387c5b3c15ed5201da73dbd3b9
stefanmaric01 <stefanmaric01@gmail.com>
2025-03-08 11:42:54 +0100

    IsAnagram

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,38 @@
-﻿static String reverse(string s)
+﻿//static String reverse(string s)
+//{
+
+//    int left = 0;
+//    int right = s.Length - 1;
+//    char[] reversed = new char[s.Length];
+
+//    while (left < s.Length)
+//    {
+//        reversed[left++] = s[right--];
+//    }
+
+//    Console.WriteLine(reversed);
+//    return reversed.ToString();
+//}
+
+//reverse("Stefan");
+
+static string isAnagram (string a, string b)
 {
 
-    int left = 0;
-    int right = s.Length - 1;
-    char[] reversed = new char[s.Length];
-
-    while (left < s.Length)
+    if (a.Length != b.Length)
     {
-        reversed[left++] = s[right--];
+       
+        return "Is not anagram";
+
     }
 
-    Console.WriteLine(reversed);
-    return reversed.ToString();
-}
+    char[] aChar = a.ToLower().ToCharArray();
+    char[] bChar = b.ToLower().ToCharArray();
 
-reverse("Stefan");
+    Array.Sort(aChar);
+    Array.Sort(bChar);
+
+   return aChar.SequenceEqual(bChar) ? "IsAnagram" : "Is not Anagram";
+
+}
+isAnagram("Ana", "naa");
